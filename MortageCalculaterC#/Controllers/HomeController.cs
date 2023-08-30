f2ae508cf38e180210728ed8ea45f9aa8dd761a4
shalant <doug.rosenberg@gmail.com>
2023-08-30 16:09:19 -0500

    initial

---
+++ MortageCalculaterC#/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MortageCalculaterC_.Models;
+using System.Diagnostics;
+
+namespace MortageCalculaterC_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Weather()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MortageCalculaterC#/Controllers/MortgageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MortageCalculaterC_.Controllers
+{
+    public class MortgageController : Controller
+    {
+        // 
+        // GET: /Mortgage/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /Mortgage/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
